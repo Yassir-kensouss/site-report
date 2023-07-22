1355d343f347108e3dfcbece60ca26314260e8cf
yqssirkensouss <y.kensouss@qalqul.io>
2023-07-22 12:59:20 -0500

    updates

--- config/contants.js
+++ config/contants.js
@@ -1,3 +1,4 @@
 const EXT_REGEX = /\.(jpeg|jpg|png|svg|webp|gif)$/;
+const HTTP_LINKS_REGEX = /^(https?:\/\/)/;
 
-module.exports = { EXT_REGEX };
+module.exports = { EXT_REGEX, HTTP_LINKS_REGEX };
--- controllers/optimizationController.js
+++ controllers/optimizationController.js
@@ -3,6 +3,9 @@ const ImagesDetails = require("../utilities/ImagesAnalytics");
 const PageLoader = require("../utilities/PageLoader");
 const puppeteer = require("puppeteer");
 const { unusedCss } = require("../utilities/styleSheetsPer");
+const fetch = require("node-fetch");
+const { HTTP_LINKS_REGEX } = require("../config/contants");
+const { fromBase64 } = require("b64-lite");
 
 exports.mediaAnalyzer = async (req, res) => {
   let pageLink = req.body.pageLink;
@@ -44,11 +47,62 @@ exports.filesAnalyzer = async (req, res) => {
     image: 0,
   };
 
-  page.on("request", request => {
+  let fontsTotalSize = 0;
+  let imageTotalSize = 0;
+  let totalCssSize = 0;
+  let totalScriptsSize = 0;
+
+  page.on("request", async request => {
+    const isLink = request.url() && HTTP_LINKS_REGEX.test(request.url());
+    const base64Data = request.url() && request.url().startsWith("data:image");
+
     requestTypes = {
       ...requestTypes,
       [request.resourceType()]: requestTypes[request.resourceType()] + 1,
     };
+
+    if (request.resourceType() === "font" && isLink) {
+      const response = await fetch(request.url());
+      const contentLength = parseInt(
+        response.headers.get("content-length"),
+        10
+      );
+      if (!isNaN(contentLength)) fontsTotalSize += contentLength;
+    }
+
+    if (request.resourceType() === "image") {
+      if (isLink) {
+        const response = await fetch(request.url());
+        const contentLength = parseInt(
+          response.headers.get("content-length"),
+          10
+        );
+        if (!isNaN(contentLength)) imageTotalSize += contentLength;
+      } else if (base64Data) {
+        const data = request.url().split(",")[1];
+        const decodedData = fromBase64(data);
+        imageTotalSize += decodedData.length;
+      }
+    }
+
+    if (request.resourceType() === "stylesheet" && isLink) {
+      const response = await fetch(request.url());
+      const contentLength = parseInt(
+        response.headers.get("content-length"),
+        10
+      );
+      if (!isNaN(contentLength)) totalCssSize += contentLength;
+    }
+
+    if (request.resourceType() === "script" && isLink) {
+      const response = await fetch(request.url());
+      const contentLength = parseInt(
+        response.headers.get("content-length"),
+        10
+      );
+      if (!isNaN(contentLength)) totalScriptsSize += contentLength;
+    }
+
     request.continue();
   });
 
@@ -65,28 +119,19 @@ exports.filesAnalyzer = async (req, res) => {
     waitUntil: "domcontentloaded",
   });
 
+  await page.waitForTimeout(6000);
+
   const { ruleUsage } = await client.send("CSS.stopRuleUsageTracking");
   const _unusedCss = unusedCss(stylesheets, ruleUsage);
-  const scriptElements = await page.$$("script");
 
-  // const cssLinks = await page.$$eval('link[rel="stylesheet"]', elements =>
-  //   elements.map(el => el.href)
-  // );
-
-  // let totalCssSize = 0;
-
-  // for (const cssLink of cssLinks) {
-  //   const response = await page.goto(cssLink, {
-  //     waitUntil: "domcontentloaded",
-  //   });
-
-  //   if (response.ok()) {
-  //     const contentLength = response.headers()["content-length"];
-  //     if (contentLength) {
-  //       totalCssSize += parseInt(contentLength);
-  //     }
-  //   }
-  // }
+  res.json({
+    fontsTotalSize,
+    imageTotalSize,
+    totalCssSize,
+    totalScriptsSize,
+    requestTypes,
+    _unusedCss,
+  });
 
   await browser.close();
   // } catch (error) {
